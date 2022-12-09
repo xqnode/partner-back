0d992f39ccfe1b1ef6963881ac7c8aee3e3d600d
xqnode <xqnode@163.com>
2022-12-09 21:15:23 +0800

    sql

--- sql/partner.sql
+++ sql/partner.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50737
  File Encoding         : 65001
 
- Date: 03/12/2022 10:40:54
+ Date: 09/12/2022 21:15:11
 */
 
 SET NAMES utf8mb4;
@@ -27,8 +27,8 @@ CREATE TABLE `dynamic`  (
   `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL COMMENT '内容',
   `imgs` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '图片',
   `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '简介',
-  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
-  `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
   `uid` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '用户标识',
   `deleted` int(255) NULL DEFAULT NULL COMMENT '删除标识',
   PRIMARY KEY (`id`) USING BTREE
@@ -51,22 +51,17 @@ CREATE TABLE `sys_user`  (
   `email` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '邮箱',
   `uid` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '用户唯一id',
   `deleted` int(1) NOT NULL DEFAULT 0 COMMENT '逻辑删除 0存在  id删除',
-  `create_time` datetime(0) NULL DEFAULT NULL,
-  `update_time` datetime(0) NULL DEFAULT NULL,
+  `create_time` datetime NULL DEFAULT NULL,
+  `update_time` datetime NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `uid_index`(`uid`) USING BTREE,
   UNIQUE INDEX `username_index`(`username`, `deleted`) USING BTREE,
   UNIQUE INDEX `email_index`(`email`, `deleted`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sys_user
 -- ----------------------------
-INSERT INTO `sys_user` VALUES (1, 'admin', 'admin', '超级管理员', '11@qq.com', 'admin123', 0, NULL, NULL);
-INSERT INTO `sys_user` VALUES (6, 'weg', '123', '李白', '22@qq.com', '4352a62213be4450a0f574138e2bd355', 6, '2022-11-29 21:27:34', '2022-11-29 21:29:04');
-INSERT INTO `sys_user` VALUES (7, 'weg', '123', '李白', '33@qq.com', 'cd1b557c26bd457dac3cfec6edd740d4', 7, '2022-11-29 21:30:28', '2022-11-29 21:32:55');
-INSERT INTO `sys_user` VALUES (8, 'weg', '123', '12123', '44@qq.com', 'b80333662477430eab52d4c18acab2ad', 0, '2022-11-29 21:34:13', '2022-11-29 21:34:13');
-INSERT INTO `sys_user` VALUES (9, 'zss', '123', 'zss', 'xqnode1@163.com', '3e6174397df343fe9d39333702434844', 0, '2022-12-01 21:13:28', '2022-12-01 21:13:28');
-INSERT INTO `sys_user` VALUES (10, 'rgg', '123', '我很帅', 'xqnode@163.com', '3bddf471eefd4b079209818fa01f0a05', 0, '2022-12-01 21:25:56', '2022-12-01 21:25:56');
+INSERT INTO `sys_user` VALUES (1, 'rgg', '$2a$10$GMrMcVqC6nmefRENrgjWXeTHd4bvkwI/lYA0PkT01A.UkPwjq5Pym', 'rgg', 'xqnode@163.com', '4918ea50c06a458f94878abe741b4f51', 0, '2022-12-09 20:08:17', '2022-12-09 20:08:17');
 
 SET FOREIGN_KEY_CHECKS = 1;
